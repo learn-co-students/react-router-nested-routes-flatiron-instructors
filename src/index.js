fc2c6c2077e1af1c6d718fd368586d100e63e877
Luke Ghenco <lukeghenco@gmail.com>
2017-02-22 08:09:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import rootReducer from './reducers'
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
-
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import rootReducer from './reducers';
 import App from './components/App';
 import MoviesPage from './containers/MoviesPage';
 import MoviesNew from './containers/MoviesNew';
 import MoviesAbout from './components/MoviesAbout';
-import MoviesShow from './containers/MoviesShow'
+import MoviesShow from './containers/MoviesShow';
 
-const initialState = {movies: [ { id: 1, title: 'A River Runs Through It' } ]}
+const initialState = {
+  movies: [ 
+    { id: 1, title: 'A River Runs Through It' } 
+  ]
+};
 
 const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
-  (<Provider store={store} >
+  <Provider store={store} >
     <Router history={browserHistory} >
       <Route path="/" component={App} />
-      <Route path='/movies' component={MoviesPage} >
+      <Route path='/movies' component={MoviesPage}>
         <Route path="/movies/:id" component={MoviesShow} />
       </Route>
     </Router>
-  </Provider>),
-document.getElementById('container'));
+  </Provider>,
+  document.getElementById('container')
+);
