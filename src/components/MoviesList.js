429f0f07943cf56610a1f8b11304c204ac7dc13a
Luke Ghenco <lukeghenco@gmail.com>
2017-02-22 07:38:29 -0500

    update starter code for MoviesPage component

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default (props) => {
+
   const movies = props.movies.map(movie => <li key={movie.id}>{movie.title}</li>);
+  
   return (
     <div>
       <div className='col-md-4'>
