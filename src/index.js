1f090d71d6072a223148965d51fff99d099adc99
Luke Ghenco <lukeghenco@gmail.com>
2017-02-22 07:41:44 -0500

    update starter code for Index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import rootReducer from './reducers'
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
-
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import rootReducer from './reducers';
 import App from './components/App';
 import MoviesPage from './containers/MoviesPage';
 import MoviesNew from './containers/MoviesNew';
 import MoviesAbout from './components/MoviesAbout';
-import MoviesShow from './containers/MoviesShow'
+import MoviesShow from './containers/MoviesShow';
 
-const initialState = {movies: [ { id: 1, title: 'A River Runs Through It' } ]}
+const initialState = {
+  movies: [ 
+    { id: 1, title: 'A River Runs Through It' } 
+  ]
+};
 
 const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
-  (<Provider store={store} >
+  <Provider store={store} >
     <Router history={browserHistory} >
       <Route path="/" component={App} />
       <Route path='/movies' component={MoviesPage}/>
     </Router>
-  </Provider>),
-document.getElementById('container'));
+  </Provider>,
+  document.getElementById('container')
+);
