6b0080c2bee0ba8703682edf7a9713645ab11e64
Luke Ghenco <lukeghenco@gmail.com>
2017-02-22 08:08:28 -0500

    Update MoviesList.js

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default (props) => {
-  const movies = props.movies;
+
+  const movies = props.movies.map(movie => 
+    <li key={movie.id}>
+      <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+    </li>
+  );
+  
   return (
     <div>
       <div className='col-md-4'>
         <ul>
-          {movies.map( movie =>
-            <li key={movie.id}>
-              <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-            </li>)}
+          {movies}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
